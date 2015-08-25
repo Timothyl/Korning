76f194913f29de2cff435fbf0df5b9317d61185f
Timothyl <timothyjlau@gmail.com>
2015-08-25 13:44:37 -0400

    first commit

---
+++ schema.sql
@@ -0,0 +1,22 @@
+-- DEFINE YOUR DATABASE SCHEMA HERE
+CREATE TABLE employee_info (
+  employee_id_primary SERIAL,
+  employee_name varchar(100),
+  employee_email varchar(100)
+);
+
+CREATE TABLE customer_info (
+  customer_name varchar(100),
+  customer_account_number_primary varchar(20)
+);
+
+CREATE TABLE purchases (
+  employee_id_foreign integer,
+  customer_account_number_foreign varchar(20),
+  product_name varchar(100),
+  sale_date date,
+  sale_amount money,
+  units_sold integer,
+  invoice_number integer,
+  invoice_frequency varchar(50)
+);
